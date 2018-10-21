93193ebb404f18209fa310390539d0a30fd7f9fa
Игорь <igor.abdulagajiev@yandex.ru>
2018-10-21 14:47:06 +0300

    первый коммит,нарезал картиночки, набросал разметку

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var server = require("browser-sync");
+
+gulp.task("style", function() {
+  gulp.src("sass/style.scss")
+  .pipe(plumber())
+  .pipe(sass())
+  .pipe(gulp.dest("css"))
+  .pipe(server.reload({stream:true}));
+});
+
+gulp.task("serve", ["style"], function() {
+  server.init({
+      server: ".",
+      notify: false
+  });
+
+  gulp.watch("sass/**/*.scss", ["style"]);
+  gulp.watch("*.html")
+  .on("change", server.reload);
+});
