7b0a34504e8757305e4a58a06a0f35e66179dff5
Игорь <igor.abdulagajiev@yandex.ru>
2019-04-26 14:54:33 +0300

    исправления

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,15 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var server = require("browser-sync");
+var postcss = require("gulp-postcss");
+var sorting = require("postcss-sorting");
+var auto = require("autoprefixer");
 
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
   .pipe(plumber())
   .pipe(sass())
+  .pipe(postcss([ auto() ]))
   .pipe(gulp.dest("css"))
   .pipe(server.reload({stream:true}));
 });
